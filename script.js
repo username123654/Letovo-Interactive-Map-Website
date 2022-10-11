62389b41bc3de32f6bf29c32350b3c0969daf5eb
ARSENIY USTINOV <ustinov.arseny.07@gmail.com>
2022-10-11 14:37:45 +0300

    New design (responsive), new files, etc.

--- script.js
+++ script.js
@@ -1,33 +1,139 @@
-var data=[["Иванов Иван", "7", "Moscow"], ["Петрова Анна", "10", "Samara"], ["Гуровиц Владимир", "12", "Moscow"], ["Иностранный Ученик", "10", "United States"]]
-var city="Moscow"
+var data = [
+    {
+        "name": "Wing Graham",
+        "region": "Leningrad Oblast",
+        "grade": "10"
+    },
+    {
+        "name": "Abraham Stone",
+        "region": "Orenburg Oblast",
+        "grade": "9"
+    },
+    {
+        "name": "Lee Stokes",
+        "region": "Tambov Oblast",
+        "grade": "9"
+    },
+    {
+        "name": "Sierra Manning",
+        "region": "Omsk Oblast",
+        "grade": "11"
+    },
+    {
+        "name": "Patricia Lucas",
+        "region": "Arkhangelsk Oblast",
+        "grade": "7"
+    },
+    {
+        "name": "Abel Caldwell",
+        "region": "Moscow City",
+        "grade": "8"
+    },
+    {
+        "name": "Wendy Huff",
+        "region": "Leningrad Oblast",
+        "grade": "9"
+    },
+    {
+        "name": "Jakeem Bell",
+        "region": "Oryol Oblast",
+        "grade": "10"
+    },
+    {
+        "name": "Rashad Gay",
+        "region": "Magadan Oblast",
+        "grade": "8"
+    },
+    {
+        "name": "Merritt Kemp",
+        "region": "Magadan Oblast",
+        "grade": "7"
+    },
+    {
+        "name": "Jemima Olsen",
+        "region": "Kostroma Oblast",
+        "grade": "10"
+    },
+    {
+        "name": "Salvador Roy",
+        "region": "Kurgan Oblast",
+        "grade": "8"
+    },
+    {
+        "name": "Dieter Herman",
+        "region": "Kaluga Oblast",
+        "grade": "9"
+    },
+    {
+        "name": "Joy Morales",
+        "region": "Samara Oblast",
+        "grade": "8"
+    },
+    {
+        "name": "Katelyn Weber",
+        "region": "Kaluga Oblast",
+        "grade": "9"
+    },
+    {
+        "name": "Cassandra Graham",
+        "region": "Tomsk Oblast",
+        "grade": "9"
+    },
+    {
+        "name": "Elton Ratliff",
+        "region": "Kaluga Oblast",
+        "grade": "8"
+    },
+    {
+        "name": "Kameko Ford",
+        "region": "Kemerovo Oblast",
+        "grade": "10"
+    },
+    {
+        "name": "Quin Calhoun",
+        "region": "Vologda Oblast",
+        "grade": "11"
+    },
+    {
+        "name": "Hedwig Salazar",
+        "region": "Samara Oblast",
+        "grade": "10"
+    }
+];
 
 
-function ToTable(data, city){
+var region="Kaluga Oblast"
+
+
+function ToTable(data, region){
     const newDiv=document.createElement("div");
-    for(let i=0; i<data.length; i++){
-        const newLine=document.createElement("div");
-        if(data[i][2]!=city){
-            continue;
+    var il;
+    for(let i = 0; i < data.length; i++){
+        if(data[i].region == region){
+            il = i;
         }
-        for(let j=0; j<data[i].length; j++){
-            const newEl=document.createElement("div");
-            const elText=document.createTextNode(data[i][j]);
-            if(j==0){ // 0 - name
-                newEl.setAttribute("id", "name");
-            }else if(j==1){ // 1 - class
-                newEl.setAttribute("id", "grade");
-            }else if(j==2){ // 2 - city
-                continue;
+    }
+    for(let i = 0; i < data.length; i++){
+        if(data[i].region == region){
+            const newLine = document.createElement("div");
+            const newEl = document.createElement("div");
+            const textName = document.createElement("div");
+            const textGrade = document.createElement("div");
+            textGrade.setAttribute("id", "grade");
+            if(i != il){
+                textGrade.setAttribute("style", "margin-bottom:50px");
             }
-            newEl.appendChild(elText);
+            textName.textContent = data[i].name;
+            textGrade.textContent = data[i].grade;
+            newEl.appendChild(textName);
+            newEl.appendChild(textGrade);
             newLine.appendChild(newEl);
+            newLine.setAttribute("id", "line");
+            newDiv.appendChild(newLine);
         }
-        newLine.setAttribute("id", "line");
-        newDiv.appendChild(newLine);
     }
     newDiv.setAttribute("id", "table");
     document.getElementById("table").replaceWith(newDiv);
 }
 
-
-ToTable(data, city)
+ToTable(data, region);
